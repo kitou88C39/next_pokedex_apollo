0a731cecbebbbe1bd25a454ca43aabdc8e081062
Ryo Kondo <info@danilog.jp>
2022-06-19 12:13:37 +0900

    画像の表示、レイアウト修正

--- pages/index.js
+++ pages/index.js
@@ -25,16 +25,24 @@ export default function Home() {
 
   return (
     <main title='NextJS PokeDex'>
-      <div className={'grid grid-cols-3 gap-3'}>
+      <div className={'grid grid-cols-3 gap-3 m-10'}>
         {pokemon.map((pokemon, index) => {
+          const paddedId = zeroPadding(index + 1, 3);
           return (
             <a key={index}>
               <Link href={`/pokemon/${pokemonId[index]}`}>
-                <a className='border-b-1 py-2 border-grey my-0 hover:shadow-md capitalize flex items-center text-xl font-bold bg-gray-100 shadow-lg'>
-                  <span className='mr-2 font-bold'>
-                    {zeroPadding(index + 1, 3)}
-                    <a>{pokemonName[index]}</a>
+                <a className='border-b-1 py-2 px-2 border-grey my-0 hover:shadow-md capitalize flex items-center text-xlbg-gray-100 shadow-lg'>
+                  <span className='mr-2'>
+                    {paddedId}
                   </span>
+                    <img
+                      src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`}
+                      alt={pokemon.name}
+                      className='w-10 h-10 mr-2 font-bold'
+                    />
+                    <span className='font-bold'>
+                      {pokemonName[index]}
+                    </span>
                 </a>
               </Link>
             </a>
